2c55debb23d0c8234792b4affc506436d493a0d6
filippov2912 <filippov2912@mail.ru>
2023-05-19 21:59:30 +1000

    Fix homework 7, edit 1.

--- src/Main.java
+++ src/Main.java
@@ -15,24 +15,23 @@ public class Main {
         int salary = 15000;
         int total = 0;
         int i = 0;
-        while (total <= 2_459_000) {
-            total = total + salary;
-            i = i + 1;
+        while (total < 2_459_000) {
+            total += salary;
+            i++;
             System.out.println("В месяц " + i + " сумма накоплений равна " + total + " рублей");
         }
-        System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
     }
 
     public static void task2() {
         System.out.println("Задача 2");
         int i = 0;
         while (i < 10) {
-            i = i + 1;
+            i++;
             System.out.print(" " + i + " ");
         }
         System.out.println(" ");
         int w = 10;
-        for (w = 10; w > 0; w = w - 1) {
+        for (w = 10; w > 0; w -= 1) {
             System.out.print(" " + w + " ");
         }
         System.out.println(" ");
@@ -42,7 +41,7 @@ public class Main {
         System.out.println("Задача 3");
         int y = 12_000_000;
         int i = 0;
-        for (i = 1; i <= 10; i = i + 1) {
+        for (i = 1; i <= 10; i++) {
             int borned = (y / 1000) * 17;
             int died = (y / 1000) * 8;
             y = (y + borned) - died;
@@ -53,27 +52,25 @@ public class Main {
     public static void task4() {
         System.out.println("Задача 4");
         int deposit = 15_000;
-        int total = 0;
-        int m = 0;
-        int depositWithPenny = deposit + (deposit / 100) * 7;
-        while (total <= 12_000_000) {
-            total = total + depositWithPenny;
-            m = m + 1;
-            System.out.println("В месяце " + m + " сумма депозита равна " + total + " рублей");
+        var penny = 0.07;
+        int i = 0;
+        while (deposit < 12_000_000) {
+            deposit += deposit * penny;
+            i++;
+            System.out.println("В месяце " + i + " сумма депозита равна " + deposit + " рублей");
         }
     }
 
     public static void task5() {
         System.out.println("Задача 5");
         int deposit = 15_000;
-        int total = 0;
-        int m = 0;
-        int depositWithPenny = deposit + (deposit / 100) * 7;
-        while (total <= 12_000_000) {
-            total = total + depositWithPenny;
-            m = m + 1;
-            if (m % 6 == 0) {
-                System.out.println("В месяце " + m + " сумма депозита равна " + total + " рублей");
+        var penny = 0.07;
+        int i = 0;
+        while (deposit < 12_000_000) {
+            deposit += deposit * penny;
+            i++;
+            if (i % 6 == 0) {
+                System.out.println("В месяце " + i + " сумма депозита равна " + deposit + " рублей");
             }
         }
     }
@@ -81,30 +78,41 @@ public class Main {
     public static void task6() {
         System.out.println("Задача 6");
         int deposit = 15_000;
-        int total = 0;
-        int m = 0;
-        int depositWithPenny = deposit + (deposit / 100) * 7;
-        while (m <= 108) {
-            total = total + depositWithPenny;
-            m = m + 1;
-            if (m % 6 == 0) {
-                System.out.println("В месяце " + m + " сумма депозита равна " + total + " рублей");
+        var penny = 0.07;
+        int i = 0;
+        while (i <= 108) {
+            deposit += deposit * penny;
+            i++;
+            if (i % 6 == 0) {
+                System.out.println("В месяце " + i + " сумма депозита равна " + deposit + " рублей");
             }
         }
     }
+
     public static void task7() {
         System.out.println("Задача 7");
         int i = 5;
-        for (i = 5; i <= 31; i = i + 7) {
+        for (i = 5; i <= 31; i+=7) {
             System.out.println("Сегодня пятница, " + i + " -е число. Необходимо подготовить отчет");
         }
     }
+
     public static void task8() {
         System.out.println("Задача 8");
-        int i = 2123;
-        for(i = 0; i <= 2123; i = i + 79) {
-            if (i >= 1823) {
-                System.out.println(i);
+        int presentYear = 2023;
+        int futureYear = 2123;
+        int pastYear = 1823;
+        // Выведем год для начала отсчета:
+        // Найдем и округлим до целого наименьшее целочисленное значение года, кратного появлению кометы:
+        int i = (pastYear / 79) + 1;
+        // start year - первый год появления кометы в искомом нам диапазоне лет
+        int startYear = i * 79;
+        startYear = 1896;
+
+        int a = 1896;
+        for (a = 1896; a <= 2123; a+=79) {
+            if (a % 79 == 0) {
+                System.out.println(a);
             }
         }
     }
